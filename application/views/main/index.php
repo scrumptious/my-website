453794a756d5c85d68048707c008d6443dc74666
scrumptious <ljasiurski@gmail.com>
2018-03-15 22:54:23 +0000

    finished styling shapesGenerator, _variables partial added, and generatorSS to landing page

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -8,7 +8,7 @@
 			<img src="assets/imgs/gameSS.png" alt="New website coming soon banner" class="img-fluid img-small-square">
 		</div>
 		<div class="col-xs-12 col-sm-6 col-md-4">
-			<img src="assets/imgs/Coming-Soon.png" alt="New website coming soon banner" class="img-fluid img-small-square">
+			<img src="assets/imgs/shapesGeneratorSS.png" alt="New website coming soon banner" class="img-fluid img-small-square">
 		</div>
 	</div>	
 	
