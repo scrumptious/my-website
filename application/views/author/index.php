0c343ae632754ea7cd9ff5bc1425ffcf69e0aa06
scrumptious <ljasiurski@gmail.com>
2018-03-18 13:31:45 +0000

    pushed author view as modified this one yesterday

--- application/views/author/index.php
+++ application/views/author/index.php
@@ -1,11 +1,19 @@
 		<div class="row"><hr></div>
-			<div class="col col-md-6 col-md-offset-3">
-				<p><span class="great_letter">I</span>n dark ages when knights..wait ups..I mean..<br>
-				Oh right! It was meants to be about me!</p><br>
+		<div class="row">
+			<div class="col-md-6 offset-md-4">
+				
+			<h1>Hello</h1>
+			</	div>
+		</div>
+		<div class="row justify-content-center">
+			<div class="col col-md-8">
+				<p class="lead"><span class="great_letter">I</span>'m <strong>Ludwik Jasiurski</strong>, web developer. Passionate about programming and computers in general.<br>
+				I do love that every day is different and there is occasion to learn something new. Thriving on challenges and natural problem solver.
+				My main skills so far are HTML, CSS (Sass) and JS, however I do have working knowledge of PHP, CodeIgniter etc. <br><br>
 
-				<p><span class="great_letter">I</span>'m <strong>Ludwik Jasiurski</strong>, a junior web developer.<br>
-				Passionate about programming and computers in general. <br>I do love that every day is different and there is occasion to learn something new. Thriving on challenges and natural problem solver.</p>
+				I moved recently to Manchester which I believe is the city of great opportunities.</p>
 			</div>
+		</div><br>
 		
 	</div><!--end of row-->
 </div><!--end of container-->
